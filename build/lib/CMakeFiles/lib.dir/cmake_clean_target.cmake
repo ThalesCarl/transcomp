a156e7a1a9d9991a3c5bb32480a9bc2c643d2527
Thales Carl Lavoratti <tclavoratti@sinmec.ufsc.br>
2017-10-23 14:49:12 -0200

    first exercise solutions generated

---
+++ build/lib/CMakeFiles/lib.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "../static_libs/Debug/liblib.a"
+)
---
+++ build/libTest/CMakeFiles/libTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libTest.dir/source/libTest.cpp.o"
+  "../bin/Debug/libTest.pdb"
+  "../bin/Debug/libTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
